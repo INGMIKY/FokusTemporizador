b572ca8845067848f5c8d8a28f0451171ccc6411
INGMIKY <migueltony9@gmail.com>
2024-04-27 20:25:55 -0600

    proyecto terminado

--- main.js
+++ main.js
@@ -10,7 +10,22 @@ const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botones = document.querySelectorAll('.app__card-button');
 const inputEnfoqueMusica = document.querySelector('#alternar-musica');
+const botonIP = document.querySelector('#start-pause');
+const textoIP = document.querySelector('#start-pause span');
+const tiempoDisplay = document.querySelector('#timer');
+
 const musica = new Audio('./sonidos/luna-rise-part-one.mp3');
+const musicaInicio = new Audio('./sonidos/play.wav');
+const musicaPause = new Audio('./sonidos/pause.mp3')
+const musicaTerminar = new Audio('./sonidos/beep.mp3');
+
+const imagenB = document.querySelector('.app__card-primary-butto-icon');
+const imagenC= './imagenes/play_arrow.png';
+const imagenP = './imagenes/pause.png';
+
+
+let tiempoTranscurrido = 1500;
+let idIntervalo = null;
 
 //para repetir la musica cuando termine
 musica.loop= true;
@@ -18,16 +33,19 @@ musica.loop= true;
 // utilizando el arrow function, trata de una función que no es necesario 
 // utilizar la palabra reservada "function" 
 botonCorto.addEventListener('click', () => {
+    tiempoTranscurrido = 300;
     cambiarContexto('descanso-corto');
     botonCorto.classList.add('active');
 });
 
 botonEnfoque.addEventListener('click', () =>{
+    tiempoTranscurrido = 1500;
     cambiarContexto('enfoque');
     botonEnfoque.classList.add('active');
 });
 
 botonLargo.addEventListener('click', () =>{
+    tiempoTranscurrido = 900;
     cambiarContexto('descanso-largo');
     botonLargo.classList.add('active');
 });
@@ -41,7 +59,7 @@ inputEnfoqueMusica.addEventListener('change', () =>{
 })
 
 function cambiarContexto(contexto){
-
+    mostrarTiempo();
     botones.forEach(function(contexto){
         contexto.classList.remove('active')
     })
@@ -67,6 +85,58 @@ function cambiarContexto(contexto){
             break;
     }
 }
+
+
+
+
+const cuentaRegresiva = () => {
+    if(tiempoTranscurrido <= 0 ){
+        reiniciar();
+        musicaTerminar.play();
+        alert('Tiempo final')
+        return 
+    }
+
+    tiempoTranscurrido -= 1;
+    mostrarTiempo();
+    // console.log("tiempo " + tiempoTranscurrido);
+}
+
+
+function iniciarPausar(){
+    
+    if(idIntervalo){
+        
+        musicaPause.play();
+        reiniciar();
+        return
+    }
+    textoIP.textContent = "Pausar";
+    imagenB.setAttribute('src', imagenP);
+    musicaInicio.play();
+    idIntervalo = setInterval(cuentaRegresiva, 1000);
+}
+
+botonIP.addEventListener('click', () =>{
+    iniciarPausar();
+    
+})
+
+
+function reiniciar(){
+    clearInterval(idIntervalo);
+    idIntervalo = null;
+    textoIP.textContent = "Comenzar";
+    imagenB.setAttribute('src', imagenC);
+}
+
+function mostrarTiempo(){
+    const tiempo = new Date(tiempoTranscurrido * 1000);
+    const tiempoFormateado = tiempo.toLocaleTimeString('es-MX',{minute:'2-digit', second:'2-digit'});
+    tiempoDisplay.innerHTML = `${tiempoFormateado}`
+}
+
+mostrarTiempo()
 
 
 //manipulaciones de elemtos: 
